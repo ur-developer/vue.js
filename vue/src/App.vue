a80ccb1927337668d451511ac96052f21415a425
Park Min Ki <ur_developer92@naver.com>
2024-02-11 22:53:42 +0900

    vue cli 강의

---
+++ vue/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <hello-world></hello-world>
+    <HelloWorld></HelloWorld>
+    <HelloWorld />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+
+var appContent = {
+  props: ['propsdata'],
+};
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    'hello-world': HelloWorld,
+    props: {
+      msg: String,
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue/src/a.vue
@@ -0,0 +1,16 @@
+var appHeader ={ template: '
+<div>header</div>
+' methods: { addNum: function() { } } }
+
+<template></template>
+
+<script>
+export default {
+  // javascript - 인스턴스옵션
+  methods: {
+    addNum: function () {},
+  },
+};
+</script>
+
+<style></style>
