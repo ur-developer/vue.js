f4ba95b3973e9877498b81dbb08048e029bd4d69
PAUL\alsrl <urdeveloper92@gmail.com>
2024-02-17 20:02:18 +0900

    인프런 강의 마지막

--- vue-form/src/App.vue
+++ vue-form/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <form v-on:submit="submitForm">
+  <form v-on:submit.prevent="submitForm">
     <div>
       <lable for="username">id: </lable>
       <input id="username" type="text" v-model="username">
@@ -13,6 +13,8 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   data : function() {
     return {
@@ -21,9 +23,22 @@ export default {
     }
   },
   methods : {
-    submitFrom: function(event) {
-      event.preventDefault();
-      console.log(this.username, this.password)
+    submitFrom: function() {
+      // event.preventDefault();
+      console.log(this.username, this.password);
+      var url = 'http://jsonplaceholder.typicode.com/users';
+      var data = {
+        username : this.username,
+        password : this.password
+      }         
+
+      axios.post(url, data)
+      .then(function(response) {
+        console.log(response);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
     }
   }
 }
